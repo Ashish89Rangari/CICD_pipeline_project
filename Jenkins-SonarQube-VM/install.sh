076c9afc5bbbd76a33cf8ec22d6fc7f4e697d10b
ASHISH-V-RANGARI <150202892+Ashish89Rangari@users.noreply.github.com>
2024-07-04 16:43:41 +0900

    Updated latest install.sh

--- Jenkins-SonarQube-VM/install.sh
+++ Jenkins-SonarQube-VM/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 sudo apt update -y
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
